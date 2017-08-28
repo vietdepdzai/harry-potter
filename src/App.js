e0612d128173329ec5bb736ac0fd330b76acd1c1
Viet <hihi>
2017-08-28 17:41:51 +0700

    Init project

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <div>Hello HELLO Viet</div>
+        </div>
+        <div className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
